28787bf0dc46fd2d5815fceba059ce4bd4ce0c23
s-vakoliuk <s.vakoliuk@gmail.com>
2021-11-24 22:05:08 +0200

    24.11.2021

--- js_task04/additional04.js
+++ js_task04/additional04.js
@@ -27,33 +27,33 @@
 //   EXAMPLE:
 //   [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
 //
-let user=[{name: 'Dima', age: 13}, {model: 'Camry'}];
-function calcFieldsObject(array){
-    let keysArray = [];
-    for (let i = 0, l = array.length; i < l; i++) {
-        let items = array[i];
-        let keys = Object.keys(items);
-        for (let j = 0, k = keys.length; j < k; j++) {
-            keysArray=keys[j];
-        }
-    }
-    return keysArray;
-};
-console.log(calcFieldsObject(user));
-
-// - Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
-//   EXAMPLE:
-// [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
 // let user=[{name: 'Dima', age: 13}, {model: 'Camry'}];
 // function calcFieldsObject(array){
-//     let valuesArray = [];
+//     let keysArray = [];
 //     for (let i = 0, l = array.length; i < l; i++) {
 //         let items = array[i];
 //         let keys = Object.keys(items);
 //         for (let j = 0, k = keys.length; j < k; j++) {
-//             valuesArray=items[keys[j]];
+//             keysArray.push(keys[j]);
 //         }
 //     }
-//     return valuesArray;
+//     return keysArray;
 // };
 // console.log(calcFieldsObject(user));
+
+// - Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
+//   EXAMPLE:
+// [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
+let user=[{name: 'Dima', age: 13}, {model: 'Camry'}];
+function calcFieldsObject(array){
+    let valuesArray = [];
+    for (let i = 0, l = array.length; i < l; i++) {
+        let items = array[i];
+        let keys = Object.keys(items);
+        for (let j = 0, k = keys.length; j < k; j++) {
+            valuesArray.push(items[keys[j]]);
+        }
+    }
+    return valuesArray;
+};
+console.log(calcFieldsObject(user));
