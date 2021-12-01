c9506326f686981996b4d3ee4e7443ce4c694336
s-vakoliuk <s.vakoliuk@gmail.com>
2021-12-01 09:11:13 +0200

    30.11.2021

--- js_task07/additional07.js
+++ js_task07/additional07.js
@@ -1,57 +1,43 @@
 // - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
 // Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
 
-function User(id, name, username, email, street,suite, city, zipcode, lat, lng, phone, website, name,catchPhrase, bs) {
-    this.id = id,
-    this.name = name,
-    this.username = username,
-    this.email = email;
-    this.address=new function (street, suite, city, zipcode) {
-        this.street = street,
-        this.suite = suite,
-        this.city = city,
-        this.zipcode = zipcode
-        this.geo=new function (lat, lng){
-            this.lat=lat,
-            this.lng=lng
-        };
-    };
-    this.phone = phone,
-    this.website = website,
-    this.company = new function (name,catchPhrase,bs){
-        this.name = name,
-        this.catchPhrase=catchPhrase,
-        this.bs=bs
-    };
-
-};
-
-
-
-let User1=new User(1,'Leanne Graham','Bret','Sincere@april.biz', 'Kulas Light','Apt. 556','Gwenborough','92998-3874','-37.3159','81.1496','1-770-736-8031 x56442','hildegard.org','Romaguera-Crona','Multi-layered client-server neural-net', 'harness real-time e-markets');
-console.log(User1);
-
-let User2 = {
-    id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincere@april.biz',
-    address: {
-        street: 'Kulas Light',
-        suite: 'Apt. 556',
-        city: 'Gwenborough',
-        zipcode: '92998-3874',
-        geo: {
-            lat: '-37.3159',
-            lng: '81.1496'
-        }
-    },
-    phone: '1-770-736-8031 x56442',
-    website: 'hildegard.org',
-    company: {
-        name: 'Romaguera-Crona',
-        catchPhrase: 'Multi-layered client-server neural-net',
-        bs: 'harness real-time e-markets'
-    }
-}
-console.log(User2);
+// function User(id, name, username, email, street,suite, city, zipcode, lat, lng, phone, website, name,catchPhrase, bs) {
+//     this.id = id,
+//     this.name = name,
+//     this.username = username,
+//     this.email = email;
+//     this.address={street:street, suite:suite, city:city, zipcode:zipcode,geo:{lat:lat, lng:lng}};
+//     this.phone = phone,
+//     this.website = website,
+//     this.company = {name:name,catchPhrase:catchPhrase,bs:bs}
+// };
+//
+//
+// let User1=new User(1,'Leanne Graham','Bret','Sincere@april.biz', 'Kulas Light','Apt. 556','Gwenborough','92998-3874','-37.3159','81.1496','1-770-736-8031 x56442','hildegard.org','Romaguera-Crona','Multi-layered client-server neural-net', 'harness real-time e-markets');
+// console.log(User1);
+//
+//
+// let User2 = {
+//     id: 1,
+//     name: 'Leanne Graham',
+//     username: 'Bret',
+//     email: 'Sincere@april.biz',
+//     address: {
+//         street: 'Kulas Light',
+//         suite: 'Apt. 556',
+//         city: 'Gwenborough',
+//         zipcode: '92998-3874',
+//         geo: {
+//             lat: '-37.3159',
+//             lng: '81.1496'
+//         }
+//     },
+//     phone: '1-770-736-8031 x56442',
+//     website: 'hildegard.org',
+//     company: {
+//         name: 'Romaguera-Crona',
+//         catchPhrase: 'Multi-layered client-server neural-net',
+//         bs: 'harness real-time e-markets'
+//     }
+// }
+// console.log(User2);
